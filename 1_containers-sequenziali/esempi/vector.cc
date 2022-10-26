21329a70c139b2dfd232b46f73f1d1414d6a3d5a
Davide <davide.vecchi.64@gmail.com>
2022-10-26 15:43:01 +0200

    commented code

--- 1_containers-sequenziali/esempi/vector.cc
+++ 1_containers-sequenziali/esempi/vector.cc
@@ -18,17 +18,17 @@ void print_vector(vector<T> &v) {
 
 
 int main() {
-	
+/*
 	cout << "\n== COSTRUTTORI ==\n";
 	vector<int> vi;
 	vector<float> vf(2);
 	vector<char> vc(4, 'c');
-	
+/*
 	cout << "\n== PRINT ==\n";
 	print_vector(vi);
 	print_vector(vf);
 	print_vector(vc);
-	
+/*
 	cout << "\n== PUSH & POP BACK ==\n";
 	print_vector(vi);
 	vi.push_back(8);
@@ -37,26 +37,26 @@ int main() {
 	print_vector(vi);
 	vi.pop_back();
 	print_vector(vi);
-	
+/*
 	cout << "\n== OFFSET [] ==\n";
 	vi[1] = 4;
 	vi[8] = 4;  // ATTENZIONE: non vengono controllati i limiti del vettore
 	cout << vi[0] << " " << vi[1] << " " << vi[2] << endl;  // ATTENZIONE: vi[2] non è più un elemento del vettore
 	print_vector(vi);
-	
+/*
 	cout << "\n== FRONT & BACK ==\n";
 	vi.push_back(2);
 	print_vector(vi);
 	cout << "front = " << vi.front() << endl;
 	cout << "back = " << vi.back() << endl;
-	
+/*
 	cout << "\n== EMPTY & CLEAR ==\n";
 	print_vector(vi);
 	cout << (vi.empty() ? "empty" : "not empty") << endl;
 	vi.clear();
 	cout << (vi.empty() ? "empty" : "not empty") << endl;
 	print_vector(vi);
-	
+/*
 	cout << "\n== RESIZE ==\n";
 	vi.push_back(2);
 	print_vector(vi);
@@ -66,7 +66,7 @@ int main() {
 	print_vector(vi);
 	vi.resize(2, 2);
 	print_vector(vi);
-	
+*/
 	cout << endl;
 	return 0;
 }
