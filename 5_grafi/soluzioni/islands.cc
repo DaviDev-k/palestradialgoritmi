7c39d2a83b93e52e42e83872318eff7226a5689d
Davide <davide.vecchi.64@gmail.com>
2023-03-22 12:31:27 +0100

    Update islands.cc

--- 5_grafi/soluzioni/islands.cc
+++ 5_grafi/soluzioni/islands.cc
@@ -28,7 +28,8 @@ public:
 
     // Merge the sets containing x and y
     void merge(int x, int y) {
-        int px = find(x), py = find(y);
+        int px = find(x);
+        int py = find(y);
         if (px != py) {
             parent[py] = px;
         }
@@ -42,57 +43,60 @@ public:
 
 
 int main() {
-
-    size_t _;
     int R, C;
-    _ = scanf("%d%d", &R, &C);
+    scanf("%d%d", &R, &C);  // leggi R e C
     
+    // la mappa delle isole può essere rappresentata in una matrice di sole due righe,
+    // dato che per ogni cella si interrogano solo la cella sopra e quella a sinistra.
     int M[2][1002];
     for (int i = 0; i < C + 2; i++) { M[0][i] = LAND; }
     M[1][0] = LAND;
     
     MergeFindSet mfs(R * C + 1);
     
-    int id = 1;
-    int count = 0;
-    bool now = 1;
+    int id = 1;     // id dell'isola
+    int count = 0;  // conteggio delle isole
+    bool now = 1;   // riga attuale della matrice
     
-    const int MAX_SIZE = 1 << 21;
-    char *line = new char[MAX_SIZE];
-    _ = fread(line, 1, MAX_SIZE, stdin);
+    const int MAX_SIZE = 1 << 21;     // MAX_SIZE = 2 ^ 21
+    char *input = new char[MAX_SIZE];  // stringa di MAX_SIZE caratteri
+    fread(input, 1, MAX_SIZE, stdin);  // legge tutto l'input
     
-    int i = 0;
+    int cursor = 0;  // posizione della lettura dell'input
     for (int r = 1; r < R + 2; r++) {
         for (int c = 1; c < C + 2; c++) {
             
-            if (r <= R && c <= C) {
+            if (r <= R && c <= C) {  // se non siamo su un bordo
+                
+                // legge il prossimo carattere '0' o '1' dell'input
                 char cell;
                 do {
-            	    cell = line[i++];
+            	    cell = input[cursor++];
                 } while (cell != '0' && cell != '1');
                 
+                // se la cella è acqua, annotalo nella matrice e passa alla successiva
                 if (cell == '0') {
                     M[now][c] = WATER;
                     continue;
                 }
 			}
 			
-            int prev_r = M[!now][c];
-            int prev_c = M[now][c - 1];
-            if (prev_r == WATER && prev_c == WATER) {
+            int prev_r = M[!now][c];     // cella sopra
+            int prev_c = M[now][c - 1];  // cella sinistra
+            if (prev_r == WATER && prev_c == WATER) {  // se sono entrambe acqua, assegna una nuova isola
                 M[now][c] = ++id;
                 count++;
-            } else {
-                bool water = prev_r == WATER || prev_c == WATER;
-                int prev_id = water ? prev_r + prev_c : prev_c;
+            } else {  // altrimenti assegna l'isola corretta
+                bool one_is_water = (prev_r == WATER || prev_c == WATER);
+                int prev_id = one_is_water ? prev_r + prev_c : prev_c;
                 M[now][c] = mfs.find(prev_id);
-                if (!water && !mfs.same(prev_r, prev_c)) {
+                if (!one_is_water && !mfs.same(prev_r, prev_c)) {  // se gli id non corrispondono unisci le isole
                     mfs.merge(prev_r, prev_c);
                     count--;
                 }
             }
         }
-        now = !now;
+        now = !now;  // aggiorna la riga corrente
     }
     printf("%d", count);
     return 0;
